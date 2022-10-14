532d896c78b93049bfd12fe293fc9e88bd78df59
Arnas Verpečinskas <arnas.verpecinskas@unity3d.com>
2022-10-14 20:32:51 +0300

    Finish movement

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -6,22 +6,57 @@ using UnityEngine.InputSystem;
 public class MovementController : MonoBehaviour
 {
     [SerializeField]
+    private int multiJumpCount = 1;
+    [SerializeField]
     private float movementSpeed = 5f;
+    [SerializeField]
+    private float jumpForce = 10f;
+    private BoxCollider2D boxCollider;
     private Rigidbody2D rigidBody;
-    private Vector2 movementInput;
+    private int jumpCount = 0;
+    private float horizontalInput;
+    private float verticalInput;
 
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     void FixedUpdate()
     {
-        rigidBody.MovePosition(rigidBody.position + movementInput * movementSpeed * Time.fixedDeltaTime);
+        rigidBody.velocity = new Vector2(horizontalInput * movementSpeed, rigidBody.velocity.y);
+        if (jumpCount > 0)
+        {
+            if (IsGrounded())
+            {
+                jumpCount = 0;
+            }
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 0.1f, LayerMask.GetMask("Ground"));
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        movementInput = context.ReadValue<Vector2>();
+        horizontalInput = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!context.performed)
+        {
+            return;
+        }
+        if (jumpCount == multiJumpCount)
+        {
+            return;
+        }
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0); //reset y velocity so the jump is from a "standing position"
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        jumpCount++;
     }
 }
