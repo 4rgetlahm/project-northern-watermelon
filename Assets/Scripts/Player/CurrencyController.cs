9be01a1114c921ea97097b9fedebf9d50108090e
Arnas Verpečinskas <arnas.verpecinskas@unity3d.com>
2022-10-16 00:22:31 +0300

    Add currency

---
+++ Assets/Scripts/Player/CurrencyController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyController : MonoBehaviour
+{
+    private static int redSouls = 0;
+    private static int blueSouls = 0;
+    private static int yellowSouls = 0;
+    private static int eyece = 0;
+    private static int fingerBurn = 0;
+
+    public static int RedSouls
+    {
+        get { return redSouls; }
+        set { redSouls = value; }
+    }
+
+    public static int BlueSouls
+    {
+        get { return blueSouls; }
+        set { blueSouls = value; }
+    }
+
+    public static int YellowSouls
+    {
+        get { return yellowSouls; }
+        set { yellowSouls = value; }
+    }
+
+    public static int Eyece
+    {
+        get { return eyece; }
+        set { eyece = value; }
+    }
+
+    public static int FingerBurn
+    {
+        get { return fingerBurn; }
+        set { fingerBurn = value; }
+    }
+
+    public void ResetCurrency()
+    {
+        redSouls = 0;
+        blueSouls = 0;
+        yellowSouls = 0;
+        eyece = 0;
+        fingerBurn = 0;
+    }
+
+}
